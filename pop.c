832a4caf05036100ecc86b2f139360e153b99c93
TonnyMpho <tonnympho1@gmail.com>
2023-06-22 14:20:54 -0700

    0x19. C - Stacks, Queues - LIFO, FIFO

--- pop.c
+++ pop.c
@@ -9,19 +9,17 @@
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *node = *stack;
 	stack_t *tmp = *stack;
 
-	if (node)
-	{
-		node = node->next;
-		if (node)
-			node->prev = NULL;
-		free(tmp);
-	}
-	else
+	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+
+	*stack = (*stack)->next;
+
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(tmp);
 }
